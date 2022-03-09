226ec4e5b5b93c5e18577f1c7ec2ff8ef17b8a67
DLinh <nogamenolife.why@gmail.com>
2022-03-09 14:19:41 +0700

    feat: eslint and prettier install

--- src/main.js
+++ src/main.js
@@ -4,9 +4,13 @@ const server = createServer((req, res) => {
   console.log('Hello World');
   console.log(res.statusCode);
   if (req.url === '/') {
-    res.write('Hello World 2');
-    res.end();
+    res.write('Hello World');
+  } else if (req.url === '/about') {
+    res.write('About Page');
+  } else {
+    res.write('404 Page');
   }
+  res.end();
 });
 server.listen(5000, () => {
   console.log('Server is running');
