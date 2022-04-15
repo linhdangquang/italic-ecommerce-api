9e310f374656e1c9f2b3fd01ee2c5143a09e41e9
DLinh <nogamenolife.why@gmail.com>
2022-04-15 15:56:19 +0700

    feat: update public

---
+++ src/routes/index.js
@@ -0,0 +1,14 @@
+import Product from './products.router.js';
+import Category from './categories.router.js';
+import Home from './home.router.js';
+import Auth from './auth.router.js';
+import HeroBanner from './herobanner.router.js';
+import Order from './order.router.js';
+export default {
+  Product,
+  Home,
+  Category,
+  Auth,
+  HeroBanner,
+  Order
+};
